fee5f517c83c49ade8c691efc0b7e01ce8d06464
Shimoheki <shimoheki@proton.me>
2024-04-11 12:50:46 -0400

    added hyprland to dotfiles

--- stow.sh
+++ stow.sh
@@ -2,3 +2,4 @@ stow zsh -t ~/
 mkdir -p ~/.config/ags && stow ags -t ~/.config/ags && ln -s /usr/share/com.github.Aylur.ags/types ~/.config/ags/types
 mkdir -p ~/.config/nvim && stow nvim -t ~/.config/nvim
 mkdir -p ~/.config/kitty && stow kitty -t ~/.config/kitty
+mkdir -p ~/.config/hypr && stow hyprland -t ~/.config/hypr
